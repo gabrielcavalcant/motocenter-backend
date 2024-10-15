d2f2ef3bfda22d6d02489add87b23c9c889f44fa
Vitor Maia <vitor.maia51@gmail.com>
2024-10-15 14:48:03 -0300

    feat: add API and other configurations

---
+++ OficinaMotorcycle.API/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using OficinaMotocenter.CrossCutting.DependencyInjection;
+using OficinaMotocenter.Persistence.Context;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Registers services in the IoC container.
+builder.Services.RegisterServices(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpContextAccessor();
+
+// Configures Serilog for logging
+builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+// Aplica as migrações automaticamente ao subir a aplicação
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>(); // Substitua "SeuDbContext" pelo nome do seu contexto de banco de dados
+    dbContext.Database.Migrate();  // Aplica as migrações pendentes
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseSerilogRequestLogging();
+
+app.Run();
